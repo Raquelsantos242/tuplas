a333230d615dc0d30774e379708075f3c3ab2edf
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2022-08-02 17:37:40 -0300

    Add files via upload

---
+++ filmes.py
@@ -0,0 +1,8 @@
+filmes= (
+( "O Filho da Noiva", 2001, "AR", 123, 7.9),
+( "Orgulho e Preconceito", 2005, "UK", 129, 7.8),
+( "A Odisséia dos Tontos", 2019, "AR", 115, 7.2)
+)
+
+print(filmes[2])
+print(filmes[2][0])
